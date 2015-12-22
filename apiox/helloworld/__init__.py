63697fbfbc0037dac6326105e04e4d9b89c6c770
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-12-22 19:57:18 +0000

    Changed for related repo pluggable-refactor changes

--- apiox/helloworld/__init__.py
+++ apiox/helloworld/__init__.py
@@ -1,3 +1,31 @@
+import asyncio
+from apiox.core.db import API
+
 __version__ = '0.1'
 
-from .definition import hook_in
+api_id = 'hello-world'
+url_prefix = '/{}/'.format(api_id)
+
+
+@asyncio.coroutine
+def setup(app):
+    from . import handlers
+
+    app.router.add_route('*', url_prefix,
+                         handlers.IndexHandler(),
+                         name='hello-world:index')
+
+
+def declare_api(session):
+    session.merge(API.from_json({
+        'id': api_id,
+        'title': 'Hello World',
+        'version': __version__,
+        'scopes': [{
+            'id': '/hello-world',
+            'title': 'Hello World!',
+            'description': 'Provides access to POST to the "Hello World!" API',
+            'grantToUser': True,
+            'requestableByAllClients': True,
+        }],
+    }))
